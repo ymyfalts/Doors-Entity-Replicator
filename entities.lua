b14732c5b923768dfbbc0860de6f21d0d0daa775
spongus <112719635+sponguss@users.noreply.github.com>
2022-11-25 22:45:18 -0300

    Create entities.lua

---
+++ entities.lua
@@ -0,0 +1,5 @@
+return {
+    AllEntities={"all", "ambush", "eyes", "glitch", "grundge", "halt", "hide", "none", "random", "rush", "screech", "seek", "shadow", "smiler", "timothy", "trashbag", "trollface"},
+    DeveloperEntities={"trollface"},
+    CustomEntities={"grundge","smiler", "trashbag"}
+}
