b3c0a04c458ab86e5a558d35d99f7078a725e033
spongus <112719635+sponguss@users.noreply.github.com>
2022-12-02 20:53:48 -0300

    Update random.lua

--- ui_cache/random.lua
+++ ui_cache/random.lua
@@ -1,3 +1,5 @@
-return function(_, EntitiesFunctions)
-    EntitiesFunctions[math.random(1,#EntitiesFunctions)](true)
+return function(_,_,_,_,_,spawnEntity, entities)
+    local selectedEntity
+    repeat selectedEntity=entities.AllEntities[math.random(1,#entities.AllEntities)] wait() until selectedEntity~="Random" and selectedEntity~="All" and selectedEntity~="None"
+    spawnEntity(selectedEntity)
 end
